8b2fab3ef99fbd347708481b6744ef2bae9f3cfc
Dits21 <43299878+Dits21@users.noreply.github.com>
2022-04-19 01:06:31 -0500

    Update sonic_csv.py

--- sonic_csv.py
+++ sonic_csv.py
@@ -7,12 +7,16 @@ import imutils
 import imageio
 import base64
 import IPython
+import csv 
+
+import pandas as pd
 
 video_size = 700, 500
 video_filename = "SONIC.mp4"
-matriz = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+
 
 def key_action():
+    #["B", "A", "MODE", "START", "UP", "DOWN", "LEFT", "RIGHT", "C", "Y", "X", "Z"]
     buttons = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     keys=pygame.key.get_pressed()
     if keys[K_LEFT]:
@@ -32,6 +36,13 @@ env.reset()
 
 done = False
 clock = pygame.time.Clock()
+#archivo
+csvfile = open('record.csv','a', newline='', encoding='utf8')
+names = ['row', 'action', 'reward']
+writer = csv.DictWriter(csvfile, fieldnames=names)
+cont=0	
+
+
 with imageio.get_writer(video_filename, fps=60) as video:
  while not done:
     clock.tick(60) 
@@ -46,12 +57,9 @@ with imageio.get_writer(video_filename, fps=60) as video:
     ob, rew, done, info = env.step(action)
     pygame.event.pump()
     print("Action ", action, "Reward ", rew)
-    matriz = np.append(matriz, [action], axis=0)
-    video.append_data(env.render(mode='rgb_array'))
+    cont=cont+1
+    writer.writerow({'row': cont, 'action': action, 'reward': rew})
 
-print(matriz)
-botones = pd.DataFrame(matriz)
-botones.to_csv('botones1.csv')
 
 
 def embed_mp4(filename):
@@ -65,5 +73,8 @@ def embed_mp4(filename):
   </video>'''.format(b64.decode())
 
   return IPython.display.HTML(tag)
+
+
+
 
 embed_mp4(video_filename)
